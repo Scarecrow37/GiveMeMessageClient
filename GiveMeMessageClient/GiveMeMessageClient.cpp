3f60e1039775b998d9bcdf8393c186d05d522be1
Suha Do <weqa123@naver.com>
2024-01-15 09:40:29 +0900

    Remove main parameter not used.

--- GiveMeMessageClient/GiveMeMessageClient.cpp
+++ GiveMeMessageClient/GiveMeMessageClient.cpp
@@ -3,7 +3,7 @@
 #include "Network/TCPClient.h"
 #include "Network/TcpSocket.h"
 
-int main(int argc, char* argv[])
+int main()
 {
     const TCPClient* Client = new TCPClient();
     const TcpSocket* Socket = Client->Connect("127.0.0.1", 5001);
